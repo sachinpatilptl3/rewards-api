61a6883b3e5be7f514319ecebbcfb44bebed73d6
Sachin Patil <sachinpatil@Sachins-MacBook-Air.local>
2026-08-07 17:42:11 +0530

    Complete review feedback and add reward utility tests

--- src/test/java/com/charter/reward/util/RewardUtilTest.java
+++ src/test/java/com/charter/reward/util/RewardUtilTest.java
@@ -2,15 +2,39 @@ package com.charter.reward.util;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RewardUtilTest {
 
     @Test
-    void calculateRewardPoints() {
+    void shouldReturnZeroForNegativeAmount() {
+        assertEquals(0,
+                RewardUtil.calculateRewardPoints(BigDecimal.valueOf(-10)));
     }
 
     @Test
-    void testCalculateRewardPoints() {
+    void shouldReturnZeroForZeroAmount() {
+        assertEquals(0,
+                RewardUtil.calculateRewardPoints(BigDecimal.ZERO));
+    }
+
+    @Test
+    void shouldReturnZeroForFifty() {
+        assertEquals(0,
+                RewardUtil.calculateRewardPoints(BigDecimal.valueOf(50)));
+    }
+
+    @Test
+    void shouldReturnFiftyForHundred() {
+        assertEquals(50,
+                RewardUtil.calculateRewardPoints(BigDecimal.valueOf(100)));
+    }
+
+    @Test
+    void shouldReturnOneHundredFiftyForOneFifty() {
+        assertEquals(150,
+                RewardUtil.calculateRewardPoints(BigDecimal.valueOf(150)));
     }
 }
